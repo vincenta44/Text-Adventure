704a25445346249659a67983566976113b1c9162
Vincent <vincent.ains4@gmail.com>
2019-04-30 11:18:43 -0400

    added unit tests

--- tests.py
+++ tests.py
@@ -91,10 +91,15 @@ assert_equal(isinstance(map['Path A']['enemy'], bool), True)
 assert_equal(isinstance(map['Path A']['neighbors'], list), True)
 
 #render(x)
-assert_equal(render(world), '\nThere is a fork in the road\nTo the right is the path,\nto the left are the woods,\nand next to you is a path to a broken down house.\nYour health is 5/5\n')
+test_world = create_world()
+assert_equal(render(test_world), '\nThere is a fork in the road\nTo the right is the path,\nto the left are the woods,\nand next to you is a path to a broken down house.\nYour health is 5/5\n')
+assert_equal(render(test_world), render_location(test_world) + render_player(test_world))
 
 #render_location(x)
-assert_equal(render_location(world), world['map']['Path A']['about'])
+test_world = create_world()
+assert_equal(render_location(test_world), test_world['map']['Path A']['about'])
+test_world['player']['location'] = 'Path F'
+assert_equal(render_location(test_world), path_f(test_world))
 
 #render_enemy(x)
 test_world = create_world()
@@ -114,6 +119,14 @@ assert_equal(render_enemy(test_world), sound_c(test_world))
 #render_player(x)
 test_world = create_world()
 assert_equal(render_player(test_world), "Your health is 5/5\n")
+test_world['player']['health'] = 4
+assert_equal(render_player(test_world), "Your health is 4/5\n")
+test_world['player']['health'] = 3
+assert_equal(render_player(test_world), "Your health is 3/5\n")
+test_world['player']['health'] = 2
+assert_equal(render_player(test_world), "Your health is 2/5\n")
+test_world['player']['health'] = 1
+assert_equal(render_player(test_world), "Your health is 1/5\n")
 
 #tunnel(x)
 test_world = create_world()
@@ -123,6 +136,7 @@ You hurl the rock at the Troll, it only get's more upset.
 You try to run, but the Troll catches up, grabs you and slams you around
 until you die.
 ''')
+test_world = create_world()
 test_world['player']['inventory'] = ["Sword"]
 assert_equal(tunnel(test_world), '''
 You wield your sword and face the troll. With little courage you scoot forward.
@@ -130,6 +144,28 @@ The Troll comes stomping at you and you dodge enough to slash it's back.
 You thrust forward, but the Troll turns around and knocks you back.
 You are cornered on the rocks. In a swift act of terror you scream and stab the troll.
 The troll is now laying at your feet...It is slain.
+''')
+assert_equal(test_world['map']['Tunnel']['about'], '''
+You can move forward from this dimly lit tunnel.
+''')
+test_world['player']['health'] = 2
+assert_equal(tunnel(test_world), '''
+You wield your sword and face the troll. With little courage you scoot forward.
+The Troll comes stomping at you and you dodge enough to slash it's back.
+You thrust forward, but the Troll turns around and knocks you back.
+You are cornered on the rocks. He slaps the sword out of your hand and crushes you.
+You died.
+''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword", "Armor"]
+assert_equal(tunnel(test_world), '''
+You wield your sword and face the Troll. Puffing out your armor, you attack.
+You slash at his feet while he smacks your chest. Your armor defends the attack.
+You slash again, but in a more lethal spot. You get it through the back.
+It kneels and dies. You have slain the troll.
+''')
+assert_equal(test_world['map']['Tunnel']['about'], '''
+You can move forward from this dimly lit tunnel.
 ''')
 
 #bear(x)
@@ -139,12 +175,112 @@ You stare at the bear in terror.
 It rushes you and attacks.
 You have no way of defending yourself and you get mauled and die.
 ''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword"]
+assert_equal(bear(test_world), '''
+You unsheathe your sword and stand your ground.
+The bear rushes at you and swiftly move to the side.
+You thrash at it with your sword. The bear turns toward you and claws your chest.
+You take a few claws to the chest, but with your sword out you are able to stab
+the bear in the head. You killed the bear.
+''')
+assert_equal(test_world['map']['Bear']['about'], '''
+The bear is slain and you may skin it.
+You may move forward.
+''')
+test_world['player']['health'] = 2
+assert_equal(bear(test_world), '''
+You unsheathe your sword and stand your ground.
+The bear rushes at you and swiftly move to the side.
+You thrash at it with your sword. The bear turns toward you and claws your chest.
+You take a few too many slashes to the chest.
+You died.
+''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword", "Armor"]
+assert_equal(bear(test_world), '''
+You bring out your sword and wave it at the bear.
+It charges you and tackles you, making you drop your sword.
+It is biting and clawing at your chest but your armor deflects the blows.
+This gives you time, you reach for your sword and stab its side.
+The bear leaps off of you and you spring up and stab it one more time.
+This finishes the bear.
+''')
+assert_equal(test_world['map']['Bear']['about'], '''
+The bear is slain and you may skin it.
+You may move forward.
+''')
 
 #path_e(x)
 test_world = create_world()
 assert_equal(path_e(test_world), '''
 The witch disintegrates you immediately.
 How did you even make it this far with no armor or weapon?
+''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword"]
+assert_equal(path_e(test_world), '''
+You run up sword in hand.
+The witch jumps and dodges you.
+You turn and slash the witches face,
+but she manifested a fireball and blew it straight through you.
+You died.
+''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword", "Armor"]
+assert_equal(path_e(test_world), '''
+A Witch springs out of the darkness...wielding spells.
+The witch stares you down. She starts glowing then attacks.
+Her spells shoot at you, but you manage to dive away.
+She shoots again this time burning your arm.
+You manage to dodge a few shots and you rush her.
+You slash her side and her face. She hits you with a spell,
+but your armor protects it. You jab her through her chest.
+The witch is dead.
+    She has a book of spells on her.
+    You can read the book and learn the spells,
+    however you will lose your sword.
+''')
+test_world['player']['health'] = 2
+assert_equal(path_e(test_world), '''
+A Witch springs out of the darkness...wielding spells.
+The witch stares you down. She starts glowing then attacks.
+Her spells shoot at you, but you manage to dive away.
+She shoots again this time burning your arm.
+You manage to dodge a few shots and you rush her.
+You jab your sword through her, she turns and casts a spell at you.
+Your armor withstands the blow and you pull your sword out,
+before you can slash she casts a fireball that melts through your armor and you.
+You died.
+''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword", "Armor"]
+test_world['player']['follower'] = True
+test_world['player']['follower num'] = 2
+assert_equal(path_e(test_world), '''
+A Witch springs out of the darkness...wielding spells.
+Benedek jumps in front to attack first.
+You bring out your sword and rush with him.
+She casts a spell at your follower, and he gets struck back.
+This distraction give you time to pursue.
+You jab your sword through her, she turns and casts a spell at you.
+Your armor withstands the blow and you pull your sword out.
+You jab again kill her.
+    She has a book of spells on her.
+    You can read the book and learn the spells,
+    however you will lose your sword.
+''')
+test_world['player']['health'] = 1
+assert_equal(path_e(test_world), '''
+A Witch springs out of the darkness...wielding spells.
+Benedek jumps in front to attack first.
+You bring out your sword and rush with him.
+She casts a spell at your follower, and he gets struck back.
+This distraction give you time to pursue.
+You jab your sword through her, she turns and casts a spell at you.
+Your armor withstands the blow and you pull your sword out,
+before you can slash she casts a fireball that melts through your armor and you.
+You died.
 ''')
 
 #dragon_fight(x)
@@ -157,6 +293,12 @@ She lays back down on you and sleeps.
 You slowly get crushed and suffocate.
 You are dead.
 ''')
+test_world = create_world()
+test_world['player']['inventory'] = ["Sword"]
+assert_equal(dragon_fight(test_world), dragon_fight_sword(test_world))
+test_world = create_world()
+test_world['player']['inventory'] = ["Spells"]
+assert_equal(dragon_fight(test_world), dragon_fight_spells(test_world))
 
 #dragon_fight_sword(x)
 test_world = create_world()
